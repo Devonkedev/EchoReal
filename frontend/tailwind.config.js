d532d24cb9f3ccf6055d3dc9349ae62ea9c8b9b8
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2025-07-27 12:40:39 +0530

    added tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+const { typewindTransforms } = require('typewind/transform');
+ 
+/** @type {import('tailwindcss').Config} \*/
+module.exports = {
+  content: {
+    files: ['./src/**/*.{js,jsx,ts,tsx}'],
+    transform: typewindTransforms,
+  },
+};
